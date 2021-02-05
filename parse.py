7dfafd04b222b934349541c754177b29f6f8c6c9
Hayley.Yan <hayley.yan@target.com>
2021-02-05 11:53:25 -0600

    add loop to grab values from json file

--- parse.py
+++ parse.py
@@ -2,6 +2,7 @@ import urllib.request
 import xmltodict, json
 import xml.etree.ElementTree as ET
 import requests
+import re
 
 
 
@@ -16,38 +17,18 @@ xml_file.write(str_type_content)
 
 # convert xml to json
 dic = dict(xmltodict.parse(str_type_content))
-print(type(dic))
+
 # # writing into the reuslt.json file
 convert_to_json = open('result.json', 'w')
 data = json.dumps(dic)
 convert_to_json.write(data)
+print(type(data))
 
 
+with open('result.json', 'r') as fin:
+    data = json.load(fin)
+    for tag in data["edmx:Edmx"]["edmx:DataServices"]["Schema"]:
+        uri = tag["@Namespace"]
+        print(uri)
+        print("--------")
 
-
-
-
-# with open('result.json', 'r') as f: 
-#     print("@Uri" , f.get('@Uri'))
-# root = ET.parse('result.xml').getroot()
-# # converting to str type
-# xmlstr = ET.tostring(root, encoding='unicode')
-
-# tree = ET.parse('result.xml')
-# root = tree.getroot()
-# print("what")
-# for reference in root.iter('Uri'):
-#     print("hi")
-#     print(reference.attrib)
-# # print(str(tree))
-# print(str(root))
-
-
-
-
-# # looping through the json file: 
-# with open('result.json', 'r') as data_file: 
-#     d = json.loads(data_file.read())
-#     print(d[''])
-
-# for uri in data.values()
