6e7ddca5851e22fa03dcb11cd48e678a32c16cbc
Hayley.Yan <hayley.yan@target.com>
2021-02-12 09:59:00 -0600

    add element tree xml parse

--- parse.py
+++ parse.py
@@ -4,30 +4,51 @@ import xml.etree.ElementTree as ET
 import requests
 import re
 
+def get_data():
+    # this reads in the link and write the content of the data into a json file
+    xml_file = open('result.txt', 'w')
+
+    # using urllib
+    url =  urllib.request.urlopen("http://redfish.dmtf.org/schemas/v1/ComputerSystem_v1.xml")
+    bytecontent = url.read()
+    str_type_content = bytecontent.decode("utf-8")
+    xml_file.write(str_type_content)
+    # print(str_type_content)
+    # print(type(str_type_content))
+    # return str_type_content
+
+    # ### if need json file, then we need this part ###
+    # # convert xml to json
+    # dic = dict(xmltodict.parse(str_type_content))
+    # print(type(dic))
+    # # # writing into the reuslt.json file
+    # convert_to_json = open('result.json', 'w')
+    # data = json.dumps(dic)
+    # convert_to_json.write(data)
+    # print(type(data))
+    # ########
+
+def parse(xmlfile):
+    tree = ET.parse(xmlfile)
+    root = tree.getroot()
+    print(root.attrib)
+
+    
+
+    
+
+def main():
+
+    get_data()
+    parse('result.txt')
 
 
-# this reads in the link and write the content of the data into a json file
-xml_file = open('result.xml', 'w')
-
-# using urllib
-url =  urllib.request.urlopen("http://redfish.dmtf.org/schemas/v1/Chassis_v1.xml")
-bytecontent = url.read()
-str_type_content = bytecontent.decode("utf-8")
-xml_file.write(str_type_content)
-
-# convert xml to json
-dic = dict(xmltodict.parse(str_type_content))
-
-# # writing into the reuslt.json file
-convert_to_json = open('result.json', 'w')
-data = json.dumps(dic)
-convert_to_json.write(data)
-
-
+if __name__ == "__main__":
+    main()
 
 # with open('result.json', 'r') as fin:
 #     data = json.load(fin)
-#     for tag in data["edmx:Edmx"]["edmx:DataServices"]["Schema"]["EntityType"]:
+#     for i in data["edmx:Edmx"]["edmx:DataServices"]["Schema"]:
 #         # namespace = tag["@Namespace"]
 #         # name = tag["EntityType"]["@Name"]
 #         # basetype = tag["EntityType"]["@BaseType"]
