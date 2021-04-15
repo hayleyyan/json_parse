785e07f275eb1f7e0f773274da1dc6ae5a17bcdf
Hayley.Yan <hayley.yan@target.com>
2021-04-15 14:07:17 -0500

    account for different case in url

--- get_redfish_version.py
+++ get_redfish_version.py
@@ -1,5 +1,6 @@
 import requests, json, re, getpass
 from requests.auth import HTTPBasicAuth
+from urllib.error import HTTPError
 
 # Use python3 to run this script
 
@@ -7,13 +8,27 @@ def main():
     bmc_input = input("Enter BMC IP address or DNS: ")
     username = input("Enter redfish username: ")
     pwd = getpass.getpass("Enter redfish password: ")
+    
+    json_case = ["JsonSchemas", "JSONSchemas"]
 
-    bmc_ip = f"https://{bmc_input}/redfish/v1/JsonSchemas"
-    response = requests.get(bmc_ip, auth=HTTPBasicAuth(username, pwd), verify=False)
-    data = response.text
-    # turn it into a dictorary so I can access the Members index
-    json_data = json.loads(data)
+    for i in range(len(json_case)):
+        try:
+            bmh_ip = f"https://{bmc_input}/redfish/v1/{json_case[i]}"
+            print("bmh IP: " + bmh_ip)
+            response = requests.get(bmh_ip, auth=HTTPBasicAuth(username, pwd), verify=False)
+            if response.status_code == 200:
+                data = response.text
+                # turn it into a dictorary so I can access the Members index
+                json_data = json.loads(data)
+                print(bmh_ip)
+                write_file(json_data)
+            else:
+                print("Response status code: " + str(response.status_code))
+                continue
+        except HTTPError:
+            print("Oops, Something went wrong.")
 
+def write_file(json_data):    
     version_file = open("versionl.txt", "w+")
     # loop through the Members list and pring out the list
     for key in json_data["Members"]:
